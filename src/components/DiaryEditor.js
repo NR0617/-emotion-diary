4d5ba88944879ddf44834f1a765c89c7e47e7c17
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-08-09 09:31:24 +0900

    feat: 컴포넌트 최적화

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "./../App";
 
@@ -15,9 +15,9 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [date, setDate] = useState(getStringDate(new Date()));
 
   const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
-  const handleClickEmotion = (emotion) => {
+  const handleClickEmotion = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
   const navigate = useNavigate();
   const handleSubmit = () => {
     if (content.length < 1) {
